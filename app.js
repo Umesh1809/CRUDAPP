30c58dbe1f688a2bad5d7b2585b88eda9ccf7c92
Umesh <dandoluumesh@gmail.com>
2025-07-23 19:20:02 +0530

    updated js file

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-require('./mongodb')
+require('./mongodb');
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const path = require('path');
 
 app.use(bodyparser.json());
 
-const detailRoute =require('./routers/router');
-app.use('/',detailRoute);
+// Serve the HTML frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
+const detailRoute = require('./routers/router');
+app.use('/', detailRoute);
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname,'app.html'));
 });
 
-
-
-app.listen(5000, (req,res) => {
-    console.log("server running...")
+app.listen(32778, '0.0.0.0', () => {
+  console.log("Server running");
 });
