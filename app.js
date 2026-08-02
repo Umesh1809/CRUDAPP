6a5753694247c386a89cd0d0e2a7f0d694e6d582
Umesh <dandoluumesh@gmail.com>
2026-08-03 05:06:54 +0530

    security

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require('./mongodb');
+require('./mongodb');
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
@@ -13,27 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 const detailRoute = require('./routers/router');
 app.use('/', detailRoute);
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname,'app.html'));
-// });
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app.html'));
+  res.sendFile(path.join(__dirname,'app.html'));
 });
 
-// // Force your code to listen on the exact port Azure expects
+// Force your code to listen on the exact port Azure expects
 const PORT = 32778; 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running globally on port ${PORT}`);
 });
-// const express = require('express');
-// const app = express();
-// const PORT = 32778;
-
-// // Serve your frontend assets
-// app.use(express.static('public')); 
-
-// // 🟢 Open the port globally immediately
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Frontend server is running smoothly on port ${PORT}`);
-// });
