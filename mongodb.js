6bea2c278636bb0b72933178d405e02a22964623
Umesh <dandoluumesh@gmail.com>
2026-08-03 03:54:11 +0530

    delte DB

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CoffeShop',(err) => {
-    if(!err) {
-        console.log('Mongodb connected')
-    }
-    else {
-        console.log('MongoDB connection error:', err) 
-    }
-});
+// mongoose.connect('mongodb://localhost:27017/CoffeShop',(err) => {
+//     if(!err) {
+//         console.log('Mongodb connected')
+//     }
+//     else {
+//         console.log('MongoDB connection error:', err) 
+//     }
+// });
